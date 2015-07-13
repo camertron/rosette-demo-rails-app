d8710f742768072a3fe350fd8fb0190cdc8b1d3a
Cameron Dutro <camertron@gmail.com>
2015-07-13 12:24:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RosetteDemoRailsApp::Application.config.secret_token = 'd25508fd3c1859d756cbd1e313d9e058765aae621f31db501329d5abb1dd0216aad54988ad120a6b3459ff6d14fdb28e4030a1c1c04d55c0124e00708033e1aa'
